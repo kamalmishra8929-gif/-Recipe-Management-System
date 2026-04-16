425b2a2828cbc2931f89a04e19c79d3289b4c74f
kamalmishra8929-gif <kamalmishra8929@gmail.com>
2026-04-16 16:48:33 +0530

    Set DEBUG to False in settings.py
    Changed DEBUG setting from True to False for production.

--- lms/settings.py
+++ lms/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-)u@!g9-+lg)-4z_ktx#dh9$q3kou+%wp($pj-^f^)p@il%u(1u'
 
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['recipy-ahu4.onrender.com', '127.0.0.1', 'localhost']
 
