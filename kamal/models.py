4772a13b647e5b0e57f50770641b0a46501109ef
kamal mishra <kamalmishra8929@gmail.com>
2026-04-16 12:28:00 +0530

    update

--- kamal/models.py
+++ kamal/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to = "recipe") 
+    image = models.ImageField(upload_to ='recipe/') 
